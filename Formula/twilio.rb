59541e2e6a0863b8aeb587616bfebddb913933c4
twilio-dx <team_interfaces+github@twilio.com>
2022-10-03 21:19:49 +0530

    changed the url for newer releases

--- Formula/twilio.rb
+++ Formula/twilio.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Twilio < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v5.0.0/twilio-v5.0.0.tar.gz"
+  url "https://rohith-prakash.github.io/test/twilio-v5.0.0/twilio-v5.0.0.tar.gz"
   version "5.0.0"
   sha256 "582e8425d459506a6b16b81410db1efbe7399d276199277961135fa35ef0a1b9"
   depends_on "node"
