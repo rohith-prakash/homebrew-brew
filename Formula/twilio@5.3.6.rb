598a049c6ac38024bfccbf9039bbdd6650ef1134
rohith-prakash <rohithprakashklm@gmail.com>
2022-10-09 04:14:34 +0000

    Release 5.3.15

--- Formula/twilio@5.3.6.rb
+++ Formula/twilio@5.3.6.rb
@@ -1,11 +1,11 @@
 require "language/node"
 
-class Twilio < Formula
+class TwilioAT536 < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://rohith-prakash.github.io/test/twilio-v5.3.15/twilio-v5.3.15.tar.gz"
-  version "5.3.15"
-  sha256 "1232a3b619a3aa6e61fae9377ea29ce6e7f9ba04893c056c6a4afd7b1b6782f9"
+  url "https://rohith-prakash.github.io/test/twilio-v5.3.6/twilio-v5.3.6.tar.gz"
+  version "5.3.6"
+  sha256 "eb2ada1eff4bc1c51005d2076dbad5c8acda0ae06a705550f5d6b479138944c1"
   depends_on "node"
 
   def install
