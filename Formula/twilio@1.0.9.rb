a8e4c3888d60323e8be8fb54386ff0daa54ccb94
rohith-prakash <rohithprakashklm@gmail.com>
2022-10-11 08:39:29 +0000

    Release 1.0.10

--- Formula/twilio@1.0.9.rb
+++ Formula/twilio@1.0.9.rb
@@ -1,11 +1,11 @@
 require "language/node"
 
-class Twilio < Formula
+class TwilioAT109 < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://rohith-prakash.github.io/test/twilio-v1.0.10/twilio-v1.0.10.tar.gz"
-  version "1.0.10"
-  sha256 "91a7e521725f09c5b62b4ea93ef27bb5dfd15935e71aad87b8f0ebab91851daa"
+  url "https://rohith-prakash.github.io/test/twilio-v1.0.9/twilio-v1.0.9.tar.gz"
+  version "1.0.9"
+  sha256 "7bcfccecac699364ef9552384e2b5c46637b4f13f1e1c59998b45363f80e606a"
   depends_on "node"
 
   def install
